3212bad4bf41528088685c91661475d0054706ca
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2022-11-11 20:35:14 -0500

    create VueToDoFooter and VueToDoList component

---
+++ src/components/VueToDoFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    <h3>{{ date }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vuetodo-footer',
+  data () {
+    return {
+      date: new Date().toLocaleString()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer{
+  margin-left: 10vw;
+  text-align: left;
+}
+</style>
---
+++ src/components/VueToDoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <li v-for="toDo in toDos" :key="toDo.name">
+      <p>{{ toDo }}</p>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueToDoList',
+  data () {
+    return {
+      toDos: [
+        {
+          name: 'Estudiar ingles'
+        },
+        {
+          name: 'Leer libro pendiente'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+
+h3 {
+  text-align: left;
+}
+</style>
